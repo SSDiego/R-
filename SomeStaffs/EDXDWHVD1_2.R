6e787562193fd5f2922be055f0dfbf2340e90310
Diego <diegosalazar.est@gmail.com>
2021-12-17 23:13:48 -0300

    algumas mudanças

---
+++ SomeStaffs/EDXDWHVD1_2.R
@@ -0,0 +1,129 @@
+#------------------------------------------------------------------------#
+
+# import a webpage into R
+library(rvest)
+url <- "https://en.wikipedia.org/wiki/Murder_in_the_United_States_by_state"
+h <- read_html(url)
+class(h)
+h
+
+tab <- h %>% html_nodes("table")
+tab <- tab[[2]]
+
+tab <- tab %>% html_table
+class(tab)
+
+tab <- tab %>% setNames(c("state", "population", "total", "murders", "gun_murders", "gun_ownership", "total_rate", "murder_rate", "gun_murder_rate"))
+head(tab)
+
+#------------------------------------------------------------------------#
+
+#EXEMPLO
+
+h <- read_html("http://www.foodnetwork.com/recipes/alton-brown/guacamole-recipe-1940609")
+recipe <- h %>% html_node(".o-AssetTitle__a-HeadlineText") %>% html_text()
+prep_time <- h %>% html_node(".m-RecipeInfo__a-Description--Total") %>% html_text()
+ingredients <- h %>% html_nodes(".o-Ingredients__a-Ingredient") %>% html_text()
+
+
+guacamole <- list(recipe, prep_time, ingredients)
+guacamole
+
+
+get_recipe <- function(url){
+  h <- read_html(url)
+  recipe <- h %>% html_node(".o-AssetTitle__a-HeadlineText") %>% html_text()
+  prep_time <- h %>% html_node(".m-RecipeInfo__a-Description--Total") %>% html_text()
+  ingredients <- h %>% html_nodes(".o-Ingredients__a-Ingredient") %>% html_text()
+  return(list(recipe = recipe, prep_time = prep_time, ingredients = ingredients))
+} 
+
+get_recipe("http://www.foodnetwork.com/recipes/food-network-kitchen/pancakes-recipe-1913844")
+
+
+#------------------------------------------------------------------------#
+
+#EXERICIO
+
+library(rvest)
+url <- "https://web.archive.org/web/20181024132313/http://www.stevetheump.com/Payrolls.htm"
+h <- read_html(url)
+
+nodes <- html_nodes(h, "table")
+
+html_text(nodes[[8]])
+
+html_table(nodes[[8]])
+
+
+#------------------------------------------------------------------------#
+
+
+# read in raw murders data from Wikipedia
+url <- "https://en.wikipedia.org/w/index.php?title=Gun_violence_in_the_United_States_by_state&direction=prev&oldid=810166167"
+murders_raw <- read_html(url) %>% 
+  html_nodes("table") %>% 
+  html_table() %>%
+  .[[1]] %>%
+  setNames(c("state", "population", "total", "murder_rate"))
+
+# inspect data and column classes
+head(murders_raw)
+class(murders_raw$population)
+class(murders_raw$total)
+
+
+#------------------------------------------------------------------------#
+
+
+s <- "Hello!"    # double quotes define a string
+s <- 'Hello!'    # single quotes define a string
+s <- `Hello`    # backquotes do not
+
+s <- "10""    # error - unclosed quotes
+s <- '10"'    # correct
+
+# cat shows what the string actually looks like inside R
+cat(s)
+
+s <- "5'"
+cat(s)
+
+# to include both single and double quotes in string, escape with \
+s <- '5'10"'    # error
+s <- "5'10""    # error
+s <- '5\'10"'    # correct
+cat(s)
+s <- "5'10\""    # correct
+cat(s)
+
+#------------------------------------------------------------------------#
+
+# murders_raw defined in web scraping video
+
+# direct conversion to numeric fails because of commas
+murders_raw$population[1:3]
+as.numeric(murders_raw$population[1:3])
+
+library(tidyverse)    # includes stringr
+
+#------------------------------------------------------------------------#
+
+# murders_raw was defined in the web scraping section
+
+# detect whether there are commas
+commas <- function(x) any(str_detect(x, ","))
+murders_raw %>% summarize_all(funs(commas))
+
+# replace commas with the empty string and convert to numeric
+test_1 <- str_replace_all(murders_raw$population, ",", "")
+test_1 <- as.numeric(test_1)
+
+# parse_number also removes commas and converts to numeric
+test_2 <- parse_number(murders_raw$population)
+identical(test_1, test_2)
+
+murders_new <- murders_raw %>% mutate_at(2:3, parse_number)
+murders_new %>% head
+
+#------------------------------------------------------------------------#
---
+++ SomeStaffs/day1.R
@@ -0,0 +1,133 @@
+library(dslabs)
+library(tidyverse)
+
+#install.packages("dslabs")  # to install a single package
+#install.packages(c("tidyverse", "dslabs")） # to install two packages at the same time
+#installed.packages() # to see the list of all installed packages
+
+# Tools>CheatsSheets
+
+#ctrl+ enter(linha) / ctrl+shit+enter(tudo)
+
+
+
+#---------------------------------------------------------------#
+
+# loading the dslabs package and the murders dataset
+library(dslabs)
+data(murders)
+
+# determining that the murders dataset is of the "data frame" class
+class(murders)
+# finding out more about the structure of the object
+str(murders)
+# showing the first 6 lines of the dataset
+head(murders)
+
+# using the accessor operator to obtain the population column
+murders$population
+# displaying the variable names in the murders dataset
+names(murders)
+# determining how many entries are in a vector
+pop <- murders$population
+length(pop)
+# vectors can be of class numeric and character
+class(pop)
+class(murders$state)
+
+# logical vectors are either TRUE or FALSE
+z <- 3 == 2
+z
+class(z)
+
+# factors are another type of class
+class(murders$region)
+# obtaining the levels of a factor
+levels(murders$region)
+
+#---------------------------------------------------------------#
+
+# We extract the population like this:
+p <- murders$population
+
+# This is how we do the same with the square brackets:
+o <- murders[["population"]] 
+
+# We can confirm these two are the same
+identical(o, p)
+
+# Use square brackets to extract `abb` from `murders` and assign it to b
+b <- murders[["abb"]]
+identical(a, b)
+
+x <- c("a", "a", "b", "b", "b", "c")
+table(x)
+
+# Write one line of code to show the number of states per region
+table(murders$region)
+
+#---------------------------------------------------------------#
+
+# We may create vectors of class numeric or character with the concatenate function
+codes <- c(380, 124, 818)
+country <- c("italy", "canada", "egypt")
+
+# We can also name the elements of a numeric vector
+# Note that the two lines of code below have the same result
+codes <- c(italy = 380, canada = 124, egypt = 818)
+codes <- c("italy" = 380, "canada" = 124, "egypt" = 818)
+
+# We can also name the elements of a numeric vector using the names() function
+codes <- c(380, 124, 818)
+country <- c("italy","canada","egypt")
+names(codes) <- country
+
+# Using square brackets is useful for subsetting to access specific elements of a vector
+codes[2]
+codes[c(1,3)]
+codes[1:2]
+
+# If the entries of a vector are named, they may be accessed by referring to their name
+codes["canada"]
+codes[c("egypt","italy")]
+
+#---------------------------------------------------------------#
+
+library(dslabs)
+data(murders)
+sort(murders$total)
+
+x <- c(31, 4, 15, 92, 65)
+x
+sort(x)    # puts elements in order
+
+index <- order(x)    # returns index that will put x in order
+x[index]    # rearranging by this index puts elements in order
+order(x)
+
+murders$state[1:10]
+murders$abb[1:10]
+
+index <- order(murders$total)
+murders$abb[index]    # order abbreviations by total murders
+
+max(murders$total)    # highest number of total murders
+i_max <- which.max(murders$total)    # index with highest number of murders
+murders$state[i_max]    # state name with highest number of total murders
+
+x <- c(31, 4, 15, 92, 65)
+x
+rank(x)    # returns ranks (smallest to largest)
+
+
+#---------------------------------------------------------------#
+
+
+# The name of the state with the maximum population is found by doing the following
+murders$state[which.max(murders$population)]
+
+# how to obtain the murder rate
+murder_rate <- murders$total / murders$population * 100000
+
+# ordering the states by murder rate, in decreasing order
+murders$state[order(murder_rate, decreasing=TRUE)]
